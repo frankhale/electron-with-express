4b897fa886a931a57f7c922ad3948ed19fc014be
Frank Hale <frankhale@gmail.com>
2017-05-14 08:21:41 -0400

    Upgrade deps

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 const electron = require("electron"),
-      app = electron.app,
-      BrowserWindow = electron.BrowserWindow;
+  app = electron.app,
+  BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     autoHideMenuBar: true,
     width: 640,
     height: 480
   });
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
   //mainWindow.webContents.openDevTools();
-  mainWindow.on("closed", function () {
+  mainWindow.on("closed", function() {
     mainWindow = null;
-  })
+  });
 }
 
 app.on("ready", createWindow);
-app.on("browser-window-created",function(e,window) {
+app.on("browser-window-created", function(e, window) {
   window.setMenu(null);
 });
 
-app.on("window-all-closed", function () {
+app.on("window-all-closed", function() {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on("activate", function () {
+app.on("activate", function() {
   if (mainWindow === null) {
     createWindow();
   }
--- start-electron.js
+++ start-electron.js
@@ -1,3 +1,3 @@
-const electron = require('electron'),
-proc = require('child_process'),
-child = proc.spawn(electron, ['.']);
+const electron = require("electron"),
+  proc = require("child_process"),
+  child = proc.spawn(electron, ["."]);
