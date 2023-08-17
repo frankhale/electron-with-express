8097296160168da03ce9c0bb214e8b03b66e2d76
Frank Hale <frankhale@gmail.com>
2023-08-17 07:47:39 -0400

    Remove better-sqlite3 and update deps
    - I'm torn on having sqlite3 installed and supported because I feel like that is a choice others will have to make if they want to support a database in their app. There are plenty of choices and removing this reduces complexity and npm packages needed to support it (electron-rebuild, etc...)

--- src/express-app.ts
+++ src/express-app.ts
@@ -5,57 +5,36 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import http from "http";
 import createError from "http-errors";
-import { name, expressPort, basePath } from "../package.json";
+import { expressPort } from "../package.json";
 import process from "process";
-import Database from "better-sqlite3";
-
-let dbPath = basePath;
 
 const app = express(),
-	router = express.Router(),
-	appName = process.execPath;
-
-if (appName.endsWith(`${name}.exe`)) {
-	dbPath = path.join("./resources/app.asar.unpacked", basePath);
-}
-
-const db = new Database(path.join(dbPath, "data.db"));
+  router = express.Router();
 
 const routes = [
-	{
-		path: "/",
-		handler: (_req: any, res: any) => res.render("index", { title: "Home" }),
-	},
-	{
-		path: "/pageTwo",
-		handler: (_req: any, res: any) =>
-			res.render("pageTwo", { title: "Page 2" }),
-	},
-	{
-		path: "/pageThree",
-		handler: (_req: any, res: any) =>
-			res.render("pageThree", { title: "Page 3" }),
-	},
-	{
-		path: "/pageFour",
-		handler: (_req: any, res: any) =>
-			res.render("pageFour", { title: "Page 4" }),
-	},
-	{
-		path: "/data",
-		handler: (_req: any, res: any) => {
-			const stmt = db.prepare("select * from People");
-			const rows = stmt.all();
-
-			res.json({
-				data: rows,
-			});
-		},
-	},
+  {
+    path: "/",
+    handler: (_req: any, res: any) => res.render("index", { title: "Home" }),
+  },
+  {
+    path: "/pageTwo",
+    handler: (_req: any, res: any) =>
+      res.render("pageTwo", { title: "Page 2" }),
+  },
+  {
+    path: "/pageThree",
+    handler: (_req: any, res: any) =>
+      res.render("pageThree", { title: "Page 3" }),
+  },
+  {
+    path: "/pageFour",
+    handler: (_req: any, res: any) =>
+      res.render("pageFour", { title: "Page 4" }),
+  }
 ];
 
 routes.forEach((route) => {
-	router.get(route.path, route.handler);
+  router.get(route.path, route.handler);
 });
 
 app.set("port", expressPort);
@@ -69,21 +48,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, path.join("..", "public"))));
 app.use("/", router);
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 app.use((err: any, req: any, res: any, _next: any) => {
-	res.locals.title = "error";
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.title = "error";
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	res.status(err.status || 500);
-	res.render("error");
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 function shutdown() {
-	console.log("Shutting down Express server...");
-	db.close();
-	server.close();
+  console.log("Shutting down Express server...");
+  server.close();
 }
 
 process.on("SIGTERM", shutdown);
@@ -92,24 +70,26 @@ process.on("SIGINT", shutdown);
 let server = http.createServer(app);
 server.listen(expressPort);
 server.on("error", (error: any) => {
-	if (error.syscall !== "listen") {
-		throw error;
-	}
+  if (error.syscall !== "listen") {
+    throw error;
+  }
 
-	var bind =
-		typeof expressPort === "string"
-			? "Pipe " + expressPort
-			: "Port " + expressPort;
+  let bind =
+    typeof expressPort === "string"
+      ? "Pipe " + expressPort
+      : "Port " + expressPort;
 
-	switch (error.code) {
-		case "EACCES":
-			console.error(bind + " requires elevated privileges");
-			process.exit(1);
-		case "EADDRINUSE":
-			console.error(bind + " is already in use");
-			process.exit(1);
-		default:
-			throw error;
-	}
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+    default:
+      throw error;
+  }
 });
 server.on("listening", () => console.log(`Listening on: ${expressPort}`));
+server.on("close", () => console.log("Express server closed."));
+server.on("error", (error: any) => console.log(error));
--- src/main.ts
+++ src/main.ts
@@ -11,98 +11,98 @@ let mainWindow: BrowserWindow | null;
 let expressPath = "./dist/src/express-app.js";
 
 if (appName.endsWith(`${name}.exe`)) {
-	expressPath = path.join("./resources/app.asar", expressPath);
+  expressPath = path.join("./resources/app.asar", expressPath);
 }
 
 function redirectOutput(x: Readable) {
-	x.on("data", function (data: any) {
-		data
-			.toString()
-			.split("\n")
-			.forEach((line: string) => {
-				if (line !== "") {
-					// regex from: http://stackoverflow.com/a/29497680/170217
-					// REGEX to Remove all ANSI colors/styles from strings
-					let serverLogEntry = line.replace(
-						/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
-						""
-					);
-					mainWindow!.webContents.send("server-log-entry", serverLogEntry);
-				}
-			});
-	});
+  x.on("data", function (data: any) {
+    data
+      .toString()
+      .split("\n")
+      .forEach((line: string) => {
+        if (line !== "") {
+          // regex from: http://stackoverflow.com/a/29497680/170217
+          // REGEX to Remove all ANSI colors/styles from strings
+          let serverLogEntry = line.replace(
+            /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+            ""
+          );
+          mainWindow!.webContents.send("server-log-entry", serverLogEntry);
+        }
+      });
+  });
 }
 
 function registerGlobalShortcuts() {
-	globalShortcut.register("CommandOrControl+Shift+L", () => {
-		mainWindow!.webContents.send("show-server-log");
-	});
+  globalShortcut.register("CommandOrControl+Shift+L", () => {
+    mainWindow!.webContents.send("show-server-log");
+  });
 }
 
 function createWindow() {
-	const expressAppProcess = spawn(appName, [expressPath], {
-		env: {
-			ELECTRON_RUN_AS_NODE: "1",
-		},
-	});
-	redirectOutput(expressAppProcess.stdout);
-	redirectOutput(expressAppProcess.stderr);
+  const expressAppProcess = spawn(appName, [expressPath], {
+    env: {
+      ELECTRON_RUN_AS_NODE: "1",
+    },
+  });
+  redirectOutput(expressAppProcess.stdout);
+  redirectOutput(expressAppProcess.stderr);
 
-	mainWindow = new BrowserWindow({
-		autoHideMenuBar: true,
-		width: 640,
-		height: 480,
-		icon: path.join(__dirname, path.join("..", "favicon.ico")),
-		webPreferences: {
-			preload: path.join(__dirname, "preload.js"),
-		},
-	});
+  mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
+    width: 640,
+    height: 480,
+    icon: path.join(__dirname, path.join("..", "favicon.ico")),
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-	mainWindow.on("closed", () => {
-		mainWindow = null;
-		expressAppProcess.kill();
-	});
-	mainWindow.on("focus", () => {
-		registerGlobalShortcuts();
-	});
-	mainWindow.on("blur", () => {
-		globalShortcut.unregisterAll();
-	});
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+    expressAppProcess.kill();
+  });
+  mainWindow.on("focus", () => {
+    registerGlobalShortcuts();
+  });
+  mainWindow.on("blur", () => {
+    globalShortcut.unregisterAll();
+  });
 
-	ipcMain.handle("get-express-app-url", () => {
-		return expressAppUrl;
-	});
+  ipcMain.handle("get-express-app-url", () => {
+    return expressAppUrl;
+  });
 
-	//mainWindow.webContents.openDevTools();
-	mainWindow.loadURL(`file://${__dirname}/../index.html`);
+  //mainWindow.webContents.openDevTools();
+  mainWindow.loadURL(`file://${__dirname}/../index.html`);
 }
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.whenReady().then(() => {
-	registerGlobalShortcuts();
-	createWindow();
+  registerGlobalShortcuts();
+  createWindow();
 
-	app.on("activate", () => {
-		if (BrowserWindow.getAllWindows().length === 0) {
-			createWindow();
-		}
-	});
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 
-	let checkServerRunning = setInterval(() => {
-		fetch(expressAppUrl)
-			.then((response: any) => {
-				if (response.status === 200) {
-					clearInterval(checkServerRunning);
-					mainWindow!.webContents.send("server-running");
-				}
-			})
-			.catch((err) => {
-				// swallow exception
-			});
-	}, 1000);
+  let checkServerRunning = setInterval(() => {
+    fetch(expressAppUrl)
+      .then((response: any) => {
+        if (response.status === 200) {
+          clearInterval(checkServerRunning);
+          mainWindow!.webContents.send("server-running");
+        }
+      })
+      .catch((err) => {
+        // swallow exception
+      });
+  }, 1000);
 });
