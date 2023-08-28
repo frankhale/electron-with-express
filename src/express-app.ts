b935e171eed5ffdaa1f2c4dcbad50e591df99482
Frank Hale <frankhale@gmail.com>
2023-08-27 22:10:54 -0400

    Upgade dependencies; Minor code changes

--- src/express-app.ts
+++ src/express-app.ts
@@ -6,58 +6,59 @@ import bodyParser from "body-parser";
 import http from "http";
 import createError from "http-errors";
 import { expressPort } from "../package.json";
-import process from "process";
 
-const app = express(),
-  router = express.Router();
+const app = express();
+const router = express.Router();
 
 const routes = [
-  {
-    path: "/",
-    handler: (_req: any, res: any) => res.render("index", { title: "Home" }),
-  },
-  {
-    path: "/pageTwo",
-    handler: (_req: any, res: any) =>
-      res.render("pageTwo", { title: "Page 2" }),
-  },
-  {
-    path: "/pageThree",
-    handler: (_req: any, res: any) =>
-      res.render("pageThree", { title: "Page 3" }),
-  },
-  {
-    path: "/pageFour",
-    handler: (_req: any, res: any) =>
-      res.render("pageFour", { title: "Page 4" }),
-  }
+  { path: "/", viewName: "index", title: "Home" },
+  { path: "/pageTwo", viewName: "pageTwo", title: "Page 2" },
+  { path: "/pageThree", viewName: "pageThree", title: "Page 3" },
+  { path: "/pageFour", viewName: "pageFour", title: "Page 4" }
 ];
 
-routes.forEach((route) => {
-  router.get(route.path, route.handler);
+routes.forEach(({ path, viewName, title }) => {
+  router.get(path, (_req, res) => res.render(viewName, { title }));
 });
 
 app.set("port", expressPort);
-app.set("views", path.join(__dirname, path.join("..", "views")));
+app.set("views", path.join(__dirname, "..", "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, path.join("..", "public"))));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/", router);
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+app.use((_req, _res, next) => next(createError(404)));
 app.use((err: any, req: any, res: any, _next: any) => {
   res.locals.title = "error";
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).render("error");
 });
+
+const server = http.createServer(app);
+
+function handleServerError(error: any) {
+  if (error.syscall !== "listen") throw error;
+
+  const bind = typeof expressPort === "string" ? `Pipe ${expressPort}` : `Port ${expressPort}`;
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
 
 function shutdown() {
   console.log("Shutting down Express server...");
@@ -67,29 +68,7 @@ function shutdown() {
 process.on("SIGTERM", shutdown);
 process.on("SIGINT", shutdown);
 
-let server = http.createServer(app);
 server.listen(expressPort);
-server.on("error", (error: any) => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  let bind =
-    typeof expressPort === "string"
-      ? "Pipe " + expressPort
-      : "Port " + expressPort;
-
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-    default:
-      throw error;
-  }
-});
+server.on("error", handleServerError);
 server.on("listening", () => console.log(`Listening on: ${expressPort}`));
 server.on("close", () => console.log("Express server closed."));
-server.on("error", (error: any) => console.log(error));
--- src/main.ts
+++ src/main.ts
@@ -8,28 +8,25 @@ import { name } from "../package.json";
 const appName = app.getPath("exe");
 const expressAppUrl = "http://127.0.0.1:3000";
 let mainWindow: BrowserWindow | null;
-let expressPath = "./dist/src/express-app.js";
 
-if (appName.endsWith(`${name}.exe`)) {
-  expressPath = path.join("./resources/app.asar", expressPath);
+const expressPath = appName.endsWith(`${name}.exe`)
+  ? path.join("./resources/app.asar", "./dist/src/express-app.js")
+  : "./dist/src/express-app.js";
+
+function stripAnsiColors(text: string): string {
+  return text.replace(
+    /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+    ""
+  );
 }
 
-function redirectOutput(x: Readable) {
-  x.on("data", function (data: any) {
-    data
-      .toString()
-      .split("\n")
-      .forEach((line: string) => {
-        if (line !== "") {
-          // regex from: http://stackoverflow.com/a/29497680/170217
-          // REGEX to Remove all ANSI colors/styles from strings
-          let serverLogEntry = line.replace(
-            /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
-            ""
-          );
-          mainWindow!.webContents.send("server-log-entry", serverLogEntry);
-        }
-      });
+function redirectOutput(stream: Readable) {
+  stream.on("data", (data) => {
+    data.toString().split("\n").forEach((line: string) => {
+      if (line !== "") {
+        mainWindow!.webContents.send("server-log-entry", stripAnsiColors(line));
+      }
+    });
   });
 }
 
@@ -39,20 +36,20 @@ function registerGlobalShortcuts() {
   });
 }
 
+function unregisterAllShortcuts() {
+  globalShortcut.unregisterAll();
+}
+
 function createWindow() {
-  const expressAppProcess = spawn(appName, [expressPath], {
-    env: {
-      ELECTRON_RUN_AS_NODE: "1",
-    },
-  });
-  redirectOutput(expressAppProcess.stdout);
-  redirectOutput(expressAppProcess.stderr);
+  const expressAppProcess = spawn(appName, [expressPath], { env: { ELECTRON_RUN_AS_NODE: "1" } });
+
+  [expressAppProcess.stdout, expressAppProcess.stderr].forEach(redirectOutput);
 
   mainWindow = new BrowserWindow({
     autoHideMenuBar: true,
     width: 640,
     height: 480,
-    icon: path.join(__dirname, path.join("..", "favicon.ico")),
+    icon: path.join(__dirname, "..", "favicon.ico"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -62,25 +59,17 @@ function createWindow() {
     mainWindow = null;
     expressAppProcess.kill();
   });
-  mainWindow.on("focus", () => {
-    registerGlobalShortcuts();
-  });
-  mainWindow.on("blur", () => {
-    globalShortcut.unregisterAll();
-  });
 
-  ipcMain.handle("get-express-app-url", () => {
-    return expressAppUrl;
-  });
+  mainWindow.on("focus", registerGlobalShortcuts);
+  mainWindow.on("blur", unregisterAllShortcuts);
 
-  //mainWindow.webContents.openDevTools();
+  ipcMain.handle("get-express-app-url", () => expressAppUrl);
+
   mainWindow.loadURL(`file://${__dirname}/../index.html`);
 }
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  if (process.platform !== "darwin") app.quit();
 });
 
 app.whenReady().then(() => {
@@ -88,21 +77,17 @@ app.whenReady().then(() => {
   createWindow();
 
   app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 
-  let checkServerRunning = setInterval(() => {
+  const checkServerRunning = setInterval(() => {
     fetch(expressAppUrl)
-      .then((response: any) => {
+      .then((response) => {
         if (response.status === 200) {
           clearInterval(checkServerRunning);
           mainWindow!.webContents.send("server-running");
         }
       })
-      .catch((err) => {
-        // swallow exception
-      });
+      .catch(() => { }); // swallow exception
   }, 1000);
 });
